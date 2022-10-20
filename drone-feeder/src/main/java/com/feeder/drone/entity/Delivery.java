5e52bffb6e4925183478fc4e2196e9939f3a6895
Thauler <thauler98@gmail.com>
2022-10-20 01:27:11 -0300

    feat(entity/Delivery): create delivery entity
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/entity/Delivery.java
+++ drone-feeder/src/main/java/com/feeder/drone/entity/Delivery.java
@@ -1,4 +1,54 @@
 package com.feeder.drone.entity;
 
-public class Delivery {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Delivery extends PanacheEntityBase {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", length = 36)
+  @NonNull
+  private UUID id;
+
+  @NonNull
+  @Column(name = "delivered_at")
+  private String deliveredAt;
+
+  @NonNull
+  private DeliveryStatus status = DeliveryStatus.PENDING;
+
+  @OneToOne
+  private Video video;
+
+  private String latitude;
+  private String longitude;
+
+  @ManyToOne
+  private Drone drone;
+
+  public Delivery() {
+  }
+
+  public Delivery(Drone drone, String latitude, String longitude) {
+    this.drone = drone;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
 }
