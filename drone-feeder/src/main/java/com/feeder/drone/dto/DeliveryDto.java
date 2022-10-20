d2632124e99a38ebf7b2d1ea4b33b383c370ba6f
Thauler <thauler98@gmail.com>
2022-10-20 01:14:58 -0300

    Changes by brunomarques feat: create DeliveryDto

--- drone-feeder/src/main/java/com/feeder/drone/dto/DeliveryDto.java
+++ drone-feeder/src/main/java/com/feeder/drone/dto/DeliveryDto.java
@@ -1,4 +1,44 @@
 package com.feeder.drone.dto;
 
+import java.util.UUID;
+
 public class DeliveryDto {
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public UUID droneID;
+
+  public String latitude;
+
+  public String longitude;
+
+  public DeliveryDto() {
+  }
+
+  public DeliveryDto(UUID droneID, String latitude, String longitude) {
+    this.droneID = droneID;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+  public UUID getDroneID() {
+    return droneID;
+  }
+
+  public void setDroneID(UUID droneID) {
+    this.droneID = droneID;
+  }
+
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
 }
