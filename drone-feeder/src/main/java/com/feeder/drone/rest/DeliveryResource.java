281e7078ffcddb6416d9b3aac81f4c3b14fb9a9e
Thauler <thauler98@gmail.com>
2022-10-20 06:06:30 -0300

    feat(rest/Delivery): add routes of PUT and DELETE
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/rest/DeliveryResource.java
+++ drone-feeder/src/main/java/com/feeder/drone/rest/DeliveryResource.java
@@ -1,12 +1,15 @@
 package com.feeder.drone.rest;
 
 import com.feeder.drone.dto.DeliveryDto;
+import com.feeder.drone.entity.Delivery;
 import com.feeder.drone.service.DeliveryService;
 import java.util.UUID;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,7 +45,31 @@ public class DeliveryResource {
   @Produces(MediaType.APPLICATION_JSON)
   public Response createDelivery(DeliveryDto deliveryDto) {
     return Response
-        .ok(deliveryService.createDelivery(deliveryDto))
+        .status(Status.CREATED)
+        .entity(deliveryService.createDelivery(deliveryDto))
         .build();
   }
+
+  @PUT
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response updateDelivery(Delivery updatesData, @PathParam("id") Long deliveryId) {
+    return Response
+        .status(Status.ACCEPTED)
+        .entity(deliveryService.update(updatesData, deliveryId))
+        .build();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response deleteDelivery(@PathParam("id") Long deliveryId) {
+    if (deliveryService.deleteDelivery(deliveryId)) {
+      return Response.noContent().build();
+    }
+    return Response.status(Status.NOT_FOUND).build();
+  }
+
 }
