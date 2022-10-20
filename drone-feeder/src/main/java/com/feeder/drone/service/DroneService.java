c7bbde9e1cf44025fcef372960d4bacc05fb4148
Thauler <thauler98@gmail.com>
2022-10-20 01:35:25 -0300

    feat(service/Drone): implements a method to list all drones
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/service/DroneService.java
+++ drone-feeder/src/main/java/com/feeder/drone/service/DroneService.java
@@ -1,6 +1,7 @@
 package com.feeder.drone.service;
 
 import com.feeder.drone.entity.Drone;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 
@@ -10,5 +11,14 @@ public class DroneService {
   @Transactional
   public void createDrone(Drone droneToAdd) {
     Drone.persist(droneToAdd);
+    droneToAdd.isPersistent();
   }
+
+  public List<Drone> getAllDrones() {
+    return Drone.listAll();
+  }
+
+    public Drone getDroneByName(String droneName) {
+      return Drone.findByName(droneName);
+    }
 }
