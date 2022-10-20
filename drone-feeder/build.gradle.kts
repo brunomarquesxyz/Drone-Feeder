7e34904fbf0fe354b61f0993dd0dcb383a168c4e
Thauler <thauler98@gmail.com>
2022-10-20 09:08:06 -0300

    build(build.gradle): add new dependencies
    jackson for serializable, java faker to mock on test, multipart to upload a file, jacoco for coverage test
    
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/build.gradle.kts
+++ drone-feeder/build.gradle.kts
@@ -17,17 +17,23 @@ dependencies {
     implementation("io.quarkus:quarkus-hibernate-orm")
     implementation("io.quarkus:quarkus-hibernate-orm-panache")
     implementation("io.quarkus:quarkus-resteasy-jaxb")
-    implementation("io.quarkus:quarkus-resteasy-jsonb")
     implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-resteasy-jackson:2.13.2.Final")
     implementation("io.quarkus:quarkus-jdbc-mysql")
     implementation("io.quarkus:quarkus-arc")
+    implementation("com.github.javafaker:javafaker:1.0.2")
+    implementation("com.h2database:h2:2.1.214")
+    implementation("io.quarkus:quarkus-resteasy-multipart:2.13.2.Final")
+    implementation("io.quarkus:quarkus-jacoco:2.13.2.Final")
+    implementation("io.github.huiaong:base64-multipartfile-starter:1.0.0-RELEASE")
+    implementation("io.quarkus:quarkus-agroal:2.13.2.Final")
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
 
     testCompileOnly("org.projectlombok:lombok:1.18.24")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
     testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("io.rest-assured:rest-assured")
+    testImplementation("io.rest-assured:rest-assured:5.2.0")
 }
 
 group = "com.feeder.drone"
