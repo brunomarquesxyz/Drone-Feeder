13d886d791dd527886d92ef22b263c86bec0f7fa
Thauler <thauler98@gmail.com>
2022-10-20 01:23:19 -0300

    refactor(entity/Drone): add attributes and change the relationship
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/entity/Drone.java
+++ drone-feeder/src/main/java/com/feeder/drone/entity/Drone.java
@@ -1,33 +1,42 @@
 package com.feeder.drone.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import java.util.List;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 public class Drone extends PanacheEntityBase {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "id")
+  @Column(name = "id", length = 36)
   @NonNull
   private UUID id;
 
   @NonNull
   @Column(unique = true)
   private String name;
+
+  @NonNull
+  private String latitude;
+
+  @NonNull
+  private String longitude;
+
+  @OneToMany(targetEntity=Delivery.class)
+  private List<Delivery> deliveries;
 
   public Drone() {}
 }
