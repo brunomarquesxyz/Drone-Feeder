cb36fe7915ccfd5708b50c8659a05f676f3bc2d5
Thauler <thauler98@gmail.com>
2022-10-21 14:59:56 -0300

    feat(Drone): add exception handlers on drone routes
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/service/DroneService.java
+++ drone-feeder/src/main/java/com/feeder/drone/service/DroneService.java
@@ -2,6 +2,7 @@ package com.feeder.drone.service;
 
 import com.feeder.drone.dto.DroneDto;
 import com.feeder.drone.entity.Drone;
+import com.feeder.drone.exceptions.ExceptionsDefinitions;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
@@ -10,22 +11,35 @@ import javax.transaction.Transactional;
 public class DroneService {
 
   @Transactional
-  public void createDrone(Drone droneToAdd) {
+  public void createDrone(Drone droneToAdd) throws ExceptionsDefinitions {
     Drone.persist(droneToAdd);
-    droneToAdd.isPersistent();
+    if(!droneToAdd.isPersistent()){
+      throw new ExceptionsDefinitions("Invalid", "Operation");
+    }
   }
 
-  public List<Drone> getAllDrones() {
-    return Drone.listAll();
+  public List<Drone> getAllDrones() throws ExceptionsDefinitions {
+    List<Drone> allDrones = Drone.listAll();
+    if (allDrones == null) {
+      throw new ExceptionsDefinitions("Bad", "Request");
+    }
+    return allDrones;
   }
 
-  public Drone getDroneByName(String droneName) {
-    return Drone.findByName(droneName);
+  public Drone getDroneByName(String droneName) throws ExceptionsDefinitions {
+    Drone droneRegistered = Drone.findByName(droneName);
+    if (droneRegistered == null) {
+      throw new ExceptionsDefinitions(droneName, "not found");
+    }
+    return droneRegistered;
   }
 
   @Transactional
-  public Drone updateDrone(Long id, DroneDto droneToUpdate) {
+  public Drone updateDrone(Long id, DroneDto droneToUpdate) throws ExceptionsDefinitions {
     Drone entity = Drone.findById(id);
+    if (entity == null) {
+      throw new ExceptionsDefinitions(id.toString(), "is not a valid ID");
+    }
 
     entity.setLatitude(droneToUpdate.getLatitude());
     entity.setLongitude(droneToUpdate.getLongitude());
@@ -36,7 +50,11 @@ public class DroneService {
   }
 
   @Transactional
-  public Boolean deleteDrone(Long droneId) {
-    return Drone.deleteById(droneId);
+  public Boolean deleteDrone(Long droneId) throws ExceptionsDefinitions {
+    boolean droneDeleted = Drone.deleteById(droneId);
+    if (droneDeleted) {
+      return droneDeleted;
+    }
+    throw new ExceptionsDefinitions(droneId.toString(), "is not a valid ID");
   }
 }
