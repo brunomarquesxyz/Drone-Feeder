d7afb96248edacd4ab5a97115f36b3d1dabc8595
Thauler <thauler98@gmail.com>
2022-10-21 14:59:40 -0300

    feat(Delivery): add exception handlers on delivery routes
    Co-authored-by: Bruno Marques <blmarques.dev@gmail.com>

--- drone-feeder/src/main/java/com/feeder/drone/service/DeliveryService.java
+++ drone-feeder/src/main/java/com/feeder/drone/service/DeliveryService.java
@@ -3,6 +3,7 @@ package com.feeder.drone.service;
 import com.feeder.drone.dto.DeliveryDto;
 import com.feeder.drone.entity.Delivery;
 import com.feeder.drone.entity.Drone;
+import com.feeder.drone.exceptions.ExceptionsDefinitions;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
@@ -11,7 +12,7 @@ import javax.transaction.Transactional;
 public class DeliveryService {
 
   @Transactional
-  public Delivery createDelivery(DeliveryDto deliveryDto) {
+  public Delivery createDelivery(DeliveryDto deliveryDto) throws ExceptionsDefinitions {
     Drone drone = Drone.find("id", deliveryDto.getDroneID()).firstResult();
     Delivery deliveryRegistred = new Delivery(
         drone,
@@ -21,21 +22,37 @@ public class DeliveryService {
 
     deliveryRegistred.setDrone(drone);
     Delivery.persist(deliveryRegistred);
-    deliveryRegistred.isPersistent();
+    if(!deliveryRegistred.isPersistent()){
+      throw new ExceptionsDefinitions("Invalid", "Operation");
+    }
+
     return deliveryRegistred;
   }
 
-  public List<Delivery> getAllDelivery() {
-    return Delivery.listAll();
+  public List<Delivery> getAllDelivery() throws ExceptionsDefinitions {
+    List<Delivery> allDeliveries = Delivery.listAll();
+    if (allDeliveries == null) {
+      throw new ExceptionsDefinitions("Bad", "Request");
+    }
+    return allDeliveries;
   }
 
-  public Delivery getDeliveryById(Long deliveryId) {
-    return Delivery.findById(deliveryId);
+  public Delivery getDeliveryById(Long deliveryId) throws ExceptionsDefinitions {
+    Delivery deliveryRegistered = Delivery.findById(deliveryId);
+    if (deliveryRegistered == null) {
+      throw new ExceptionsDefinitions(deliveryId.toString(), "is not a valid ID");
+
+    }
+    return deliveryRegistered;
   }
 
   @Transactional
-  public Delivery update(Delivery updatesData, Long deliveryId) {
+  public Delivery update(Delivery updatesData, Long deliveryId) throws ExceptionsDefinitions {
     Delivery toUpdate = Delivery.findById(deliveryId);
+
+    if (toUpdate == null) {
+      throw new ExceptionsDefinitions(deliveryId.toString(), "is not a valid ID");
+    }
 
     toUpdate.setStatus(updatesData.getStatus());
     toUpdate.setLatitude(updatesData.getLatitude());
@@ -47,7 +64,11 @@ public class DeliveryService {
   }
 
   @Transactional
-  public Boolean deleteDelivery(Long deliveryID) {
-    return Delivery.deleteById(deliveryID);
+  public Boolean deleteDelivery(Long deliveryID) throws ExceptionsDefinitions {
+    boolean deliveryDeleted = Delivery.deleteById(deliveryID);
+    if (deliveryDeleted) {
+      return deliveryDeleted;
+    }
+    throw new ExceptionsDefinitions(deliveryID.toString(), "is not a valid ID");
   }
 }
