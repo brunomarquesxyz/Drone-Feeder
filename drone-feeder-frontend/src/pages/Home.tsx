6115df6c7743ffff37fd9aaf66ecf00fdb2c5f08
Bruno Marques <blmarques.dev@gmail.com>
2022-10-23 17:19:05 -0300

    build(next-material-ui): config nextjs to using Material-UI

---
+++ drone-feeder-frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Home: React.FC = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>Welcome to Drone Feeder</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ drone-feeder-frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>Welcome to Drone Feeder</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;
